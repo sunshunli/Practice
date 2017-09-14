76a7c3ad1c90d71d216d9c09a05c9a8a31942446
sunshunli <sunshunli@hotmail.com>
2017-09-14 17:36:00 +0800

    20170914

---
+++ GoTopBtn/js/js.js
@@ -0,0 +1,26 @@
+!function(window, document, $, undefined) {
+
+    var goTop = function() {
+        $(window).on('scroll', function() {
+            var $this = $(this),
+                st = $this.scrollTop(),
+                wHeight = $this.height();
+
+            if (st > wHeight) {
+                $('.goTop').slideDown();
+            } else {
+                $('.goTop').slideUp();
+
+            }
+        });
+
+        $('.goTop').on('click', function() {
+            $('html').animate({
+                scrollTop: 0
+            }, 300);
+        });
+    };
+
+    goTop();
+
+}(window, document, jQuery);
